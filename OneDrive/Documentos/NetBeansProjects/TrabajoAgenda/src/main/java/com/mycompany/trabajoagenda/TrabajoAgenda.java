1e02ff4231efa0c612e34084e2becac64265d581
Odaliz <75827085@continental.edu.pe>
2023-04-23 23:14:31 -0500

    CodigoPrueba

---
+++ OneDrive/Documentos/NetBeansProjects/TrabajoAgenda/src/main/java/com/mycompany/trabajoagenda/TrabajoAgenda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajoagenda;
+
+/**
+ *
+ * @author odali
+ */
+public class TrabajoAgenda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
