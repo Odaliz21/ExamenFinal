6ad0a4785f81ddb25447d456ae1f63e88973975a
Odaliz21 <128641460+Odaliz21@users.noreply.github.com>
2023-07-02 15:36:08 -0500

    Código Incorrecto

--- RegistroMensualidad_1/RegistroMensualidad/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.registromensualidad {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens mensualidad.control to javafx.fxml;
-    exports mensualidad.control;
-}
