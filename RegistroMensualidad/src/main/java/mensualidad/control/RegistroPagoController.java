3664582fd67b79fe46a6a130b0f08ba3692dff2f
LOURDESMAQCONTI <128641797+Lourdesconti@users.noreply.github.com>
2023-07-03 17:53:10 -0500

    Implementación JPA y Jasper

---
+++ RegistroMensualidad/src/main/java/mensualidad/control/RegistroPagoController.java
@@ -0,0 +1,81 @@
+package mensualidad.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import mensualidad.control.jpa.jpaEstudiante;
+import mensualidad.control.jpa.jpaPago;
+import mensualidad.modelo.Estudiante;
+import mensualidad.modelo.Pago;
+/**
+ * FXML Controller class
+ *
+ * @author lourdes
+ */
+
+public class RegistroPagoController implements Initializable {
+
+    @FXML
+    private TextField idPagosField;
+    @FXML
+    private TextField gradoField;
+
+    @FXML
+    private TextField IdEstudianteField;
+    @FXML
+    private TextField montoPagarField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    private void btnPagar(ActionEvent event) {
+        String grado = gradoField.getText();
+        double montoPagar = Double.parseDouble(montoPagarField.getText());
+
+        int idEstudiante = Integer.parseInt(IdEstudianteField.getText());
+
+        // Obtener el estudiante por su ID
+        jpaEstudiante estudianteDAO = new jpaEstudiante();
+        estudianteDAO.cargarEstudiantes();
+
+        Estudiante estudiante = null;
+        for (Estudiante e : estudianteDAO.getDatoEstudiantes()) {
+            if (e.getIdEstudiante() == idEstudiante) {
+                estudiante = e;
+                break;
+            }
+        }
+
+        if (estudiante != null) {
+            // Crear una instancia de Pago con el estudiante y otros datos
+            Pago pago = new Pago(grado, montoPagar, estudiante);
+
+            // Crear una instancia de jpaPago
+            jpaPago pagoDAO = new jpaPago();
+
+            // Insertar el pago en la base de datos
+            pagoDAO.insertarPago(pago);
+
+            // Mostrar un mensaje de éxito o realizar alguna otra acción
+            System.out.println("Pago registrado correctamente.");
+        } else {
+            System.out.println("No se encontró un estudiante con el ID proporcionado.");
+        }        
+    }
+
+    @FXML
+    private void btnSalir(ActionEvent event) {
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.close();
+    }
+
+}
---
+++ RegistroMensualidad/src/main/java/mensualidad/control/reporte/Reporte.java
@@ -0,0 +1,61 @@
+package mensualidad.control.reporte;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import javafx.event.ActionEvent;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.swing.JFrame;
+import mensualidad.control.PagoController;
+import net.sf.jasperreports.swing.JRViewer;
+
+public class Reporte{
+
+     private String reporte; 
+  
+     public Reporte(String reporte){ 
+         this.reporte = reporte; 
+     } 
+  
+     public void generarReporte(){ 
+         try { 
+             // Obtiene el EntityManager a través de la unidad de persistencia "BaseDatos" 
+             EntityManager entityManager = Persistence.createEntityManagerFactory("BaseDatos").createEntityManager(); 
+  
+             // Obtiene la conexión a partir del EntityManager 
+             Connection conn = entityManager.unwrap(Connection.class); 
+  
+             // Código para generar el informe utilizando JasperReports 
+             String rutaJRXML = "C:\\Users\\Hp\\OneDrive\\Documentos\\NetBeansProjects\\RegistroMensualidad\\src\\main\\java\\mensualidad\\control\\reporte\\Pago.jrxml"; 
+             File archivoJRXML = new File(rutaJRXML); 
+             InputStream dir = new FileInputStream(archivoJRXML); 
+             JasperReport jr = JasperCompileManager.compileReport(dir); 
+             JasperPrint jp = JasperFillManager.fillReport(jr, null, conn); 
+  
+             // Muestra el informe en un JFrame 
+             JRViewer viewer = new JRViewer(jp); 
+             JFrame frame = new JFrame("Reporte"); 
+             frame.getContentPane().add(viewer); 
+             frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+             frame.pack(); 
+             frame.setVisible(true); 
+  
+             // Cierra el EntityManager 
+             entityManager.close(); 
+         } catch (Exception ex) { 
+             System.out.println("Error al generar el reporte " + this.reporte + ": " + ex); 
+         } 
+     }     
+
+}
